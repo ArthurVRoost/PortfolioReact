2e688c6bf0f198d22104bca23c02cf6aed2b2ac0
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-06-11 15:22:44 +0200

    ajout layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Nav from './components/nav/Nav'
 import Home from './pages/Home/Home'
+import Layout from './components/Layout'
+import AllProjects from './pages/projets/AllProjects'
 
 function App() {
   
 
   return (
     <>
-      <Nav/>
-      <Home/>
-      <Footer/>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path='/allProjects' element={<AllProjects/>}/>
+        </Route>
+      </Routes>
     </>
   )
 }
---
+++ src/pages/projets/AllProjects.jsx
@@ -0,0 +1,9 @@
+import'./allprojects.css'
+
+export default function AllProjects() {
+    return(
+        <>
+        <h1>PAGE EN COURS DE TRAVAIL</h1>
+        </>
+    )
+}
