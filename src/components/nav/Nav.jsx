5823e886a525352f87dd57b4374b94a3e8d16a1d
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-06-11 09:56:48 +0200

    header nav complet

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,21 +1,31 @@
 import './nav.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons';
+import PPNAV from '/src/assets/img/pp2-removebg-preview.png'
 export default function Nav() {
     return(
         <>
-        <nav>
-            <h2 className='navH2'>ARTHUR.</h2>
-            <ul id='sideMenu'>
-                <li><a href="#header">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <FontAwesomeIcon icon={faXmark} />
-            </ul>
-            <FontAwesomeIcon icon={faBars} />
-        </nav>
+        <header id='header'>
+            <div className="container">
+                <nav>
+                    <h2 className='navH2'>ARTHUR.</h2>
+                    <ul id='sideMenu'>
+                        <li><a href="#header">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        {/* <FontAwesomeIcon icon={faXmark} /> */}
+                    </ul>
+                    {/* <FontAwesomeIcon icon={faBars} /> */}
+                </nav>
+                <div className="header-text">
+                    <p>Full-Stack Developper</p>
+                    <h1>Hi, I'm <span>Arthur</span> <br/> Van Roost from Belgium</h1>
+                </div>
+                <img className='imgHeader' src={PPNAV} alt="" />
+            </div>
+        </header>
         </>
     )
 }
