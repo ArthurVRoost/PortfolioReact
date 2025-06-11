c245ef960e9b94f7e99d9d4320052af33e76de60
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-06-11 15:15:51 +0200

    footer html css

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css'
+
+export default function Footer() {
+    return(
+        <>
+        <footer >
+            <p >&copy; Made by <span>ARTHUR</span> Van Roost. 2025. </p>
+        </footer>
+        </>
+    )
+}
