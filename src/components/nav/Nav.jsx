bb3e7e3652515cb141a1e050f459239db9813f9e
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-06-11 14:18:09 +0200

    skills html css

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -12,7 +12,7 @@ export default function Nav() {
                     <ul id='sideMenu'>
                         <li><a href="#header">Home</a></li>
                         <li><a href="#about">About</a></li>
-                        <li><a href="#services">Services</a></li>
+                        <li><a href="#skills">Skills</a></li>
                         <li><a href="#portfolio">Portfolio</a></li>
                         <li><a href="#contact">Contact</a></li>
                         {/* <FontAwesomeIcon icon={faXmark} /> */}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import './home.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  } from '@fortawesome/free-solid-svg-icons';
-import {  } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5,faReact, faSquareJs,faCss, faSass, faBootstrap } from '@fortawesome/free-brands-svg-icons';
 import PPSECTION1 from '/src/assets/img/PP.jpg'
 export default function Home() {
     return(
         <>
+        {/* SECTION1 */}
         <section id='about'>
             <div className="container">
                 <div className="row">
@@ -38,6 +39,50 @@ export default function Home() {
                                 <li><span>Back-End</span><br/>2 month training at MolenGeek</li>
                             </ul>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {/* SECTION 2 */}
+        <section id='skills'>
+            <div className="container">
+                <h1 className='subtitle skillsH1'>My Skills</h1>
+                <div className="skills-list slide-left">
+                    <div className='divSkills'>
+                        <FontAwesomeIcon className='skillsI' icon={faHtml5} />
+                        <h2>HTML</h2>
+                        <p>HTML (HyperText Markup Language) is the standard language used to create and structure content on the web. It defines elements such as headings, paragraphs, links, images, and more for web pages. To learn the basics, don't hesitate to use the contact form below.</p>
+                        <a href="https://developer.mozilla.org/fr/docs/Web/HTML">Learn More</a>
+                    </div>
+                     <div className='divSkills'>
+                        <FontAwesomeIcon className='skillsI' icon={faCss} />
+                        <h2>CSS</h2>
+                        <p>CSS (Cascading Style Sheets) is used to style HTML elements on web pages. It controls layout, colors, fonts, and spacing, enhancing visual presentation. CSS helps separate content from design, making websites more flexible, organized, and easier to maintain.</p>
+                        <a href="https://developer.mozilla.org/fr/docs/Web/CSS">Learn More</a>
+                    </div>
+                     <div className='divSkills'>
+                        <FontAwesomeIcon className='skillsI' icon={faSass} />
+                        <h2>SASS</h2>
+                        <p>SASS (Syntactically Awesome Stylesheets) is a CSS preprocessor that adds features like variables, nesting, mixins, and functions to make styling more efficient and reusable. It compiles into standard CSS, helping developers write cleaner, more organized, and maintainable style code.</p>
+                        <a href="https://sass-lang.com/">Learn More</a>
+                    </div>
+                    <div className='divSkills'>
+                        <FontAwesomeIcon className='skillsI' icon={faBootstrap} />
+                        <h2>BOOTSTRAP</h2>
+                        <p>Bootstrap is a popular front-end framework for building responsive, mobile-first websites quickly. It includes pre-designed HTML, CSS, and JavaScript components like buttons, grids, and navigation bars, allowing developers to create consistent, modern layouts without writing extensive custom code.</p>
+                        <a href="https://sass-lang.com/">Learn More</a>
+                    </div>
+                    <div className='divSkills'>
+                        <FontAwesomeIcon className='skillsI' icon={faReact} />
+                        <h2>REACT</h2>
+                        <p>ReactJS is a JavaScript library for building user interfaces, developed by Facebook. It uses components to create interactive UIs efficiently, with a virtual DOM for fast updates. React is widely used for single-page applications and dynamic web apps.</p>
+                        <a href="https://react.dev/">Learn More</a>
+                    </div>
+                    <div className='divSkills'>
+                        <FontAwesomeIcon className='skillsI' icon={faSquareJs} />
+                        <h2>JAVASCRIPT</h2>
+                        <p>JavaScript (JS) is a programming language used to create interactive and dynamic content on websites. It enables features like animations, form validation, and real-time updates without reloading the page. To learn the basics, don't hesitate to use the contact form below.</p>
+                        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">Learn More</a>
                     </div>
                 </div>
             </div>
